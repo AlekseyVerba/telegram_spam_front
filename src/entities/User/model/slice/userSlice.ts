c589352c832c612771424e53026adf3e5cbc4d41
verba <leha.verba.01@mail.ru>
2024-03-21 00:28:51 +0800

    test

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 import { UserSchema } from '../types/userSchema'
 import { KEY_USER_IN_STORAGE } from 'shared/const/localstorage'
+import { $api } from 'shared/api/api'
 
 
 const initialState: UserSchema = {
@@ -14,6 +15,7 @@ const UserSlice = createSlice({
     reducers: {
         setAuthData: (state, action: PayloadAction<Components.Schemas.AuthUserWithJwtToken>) => {
             state.authData = action.payload
+            $api.defaults.headers.common['Authorization'] = `Bearer ${action.payload.token}`
         },
         setInitied: (state, action: PayloadAction<boolean>) => {
             state._inited = action.payload
@@ -21,6 +23,8 @@ const UserSlice = createSlice({
         logout: (state) => {
             state.authData = undefined
             localStorage.removeItem(KEY_USER_IN_STORAGE)
+
+            $api.defaults.headers.common['Authorization'] = `Bearer`
         }
     },
     extraReducers: (builder) => {
--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import { KEY_USER_IN_STORAGE } from 'shared/const/localstorage';
 
-console.log('aaaa')
-console.log(process.env)
-
 export const $api = axios.create({
     baseURL: `${process.env.BACKEND_URL || 'http://91.186.196.84:3010'}/api/`,
-    headers: {
-        authorization: `Bearer ${localStorage.getItem(KEY_USER_IN_STORAGE) || ''}`,
-    },
 })
+
+$api.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(KEY_USER_IN_STORAGE) || ''}`
