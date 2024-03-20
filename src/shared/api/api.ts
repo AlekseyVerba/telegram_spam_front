02b696e31b93010fcc713e9b4ab4ca3e9a339161
verba <leha.verba.01@mail.ru>
2024-03-20 23:46:08 +0800

    test

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { KEY_USER_IN_STORAGE } from 'shared/const/localstorage';
 
+console.log('aaaa')
+console.log(process.env)
+
 export const $api = axios.create({
-    baseURL: `${process.env.BACKEND_URL || 'http://localhost:3010'}/api/`,
+    baseURL: `${process.env.BACKEND_URL || 'http://91.186.196.84:3010'}/api/`,
     headers: {
         authorization: `Bearer ${localStorage.getItem(KEY_USER_IN_STORAGE) || ''}`,
     },
